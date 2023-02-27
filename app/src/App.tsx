7c19f8b45433653547d2c2316001a2d3c327e0ac
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-02-27 11:22:37 -0300

    dobra 3 mobile completa

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Dobra1 from './Dobra1';
 import Dobra2 from './Dobra2';
+import Dobra3 from './Dobra3';
 
 function App() {
   return (
     <>
       <Dobra1/>
       <Dobra2/>
+      <Dobra3/>
     </>
   );
 }
