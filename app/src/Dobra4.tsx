076a80afffa32f0767a3b73684d0a634552322c3
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-02-28 20:41:40 -0300

    dobra 4 e 5 feitas

--- app/src/Dobra4.tsx
+++ app/src/Dobra4.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Dobra4 () {
     return (
         <div className="flex flex-col justify-center items-center py-10 text-center w-full gap-4">
-            <h3 className="font-bold text-prim text-lg">O que nossos clientes dizem:</h3>
+            <h3 className="font-bold text-prim text-lg tablet:text-4xl">O que nossos clientes dizem:</h3>
 
-            <div className="w-full h-40 bg-slate-600"/>
-            <p className='font-light text-[9px]'>Avaliação totalizada Google <span className="font-bold text-black"> 4.9 de 5</span>, com base em <span className="font-bold text-black">155 avaliações.</span></p>
+            <div className="w-full tablet:w-3/4 h-40 tablet:h-56 bg-[#D9D9D9]"/>
+            <p className='font-light text-[9px] tablet:text-[13px]'>Avaliação totalizada Google <span className="font-bold text-black"> 4.9 de 5</span>, com base em <span className="font-bold text-black">155 avaliações.</span></p>
         </div>
     )
 }
--- app/src/Dobra5.tsx
+++ app/src/Dobra5.tsx
@@ -8,9 +8,9 @@ import { BsTelephoneFill } from 'react-icons/bs'
 
 function Dobra5() {
     return(
-        <div className="flex flex-row justify-center items-end gap-4 bg-[rgba(217,_217,_217,_.3)] pt-6">
+        <div className="flex flex-row justify-center items-end gap-4 tablet:gap-[13%] bg-[rgba(217,_217,_217,_.3)] pt-6">
                 <picture>
-                    <source media="(min-width:800px)" srcSet={itemb} width='auto' />
+                    <source media="(min-width:640px)" srcSet={itemb} width='200px' />
                     <img src={itemb} alt="Menina jovem com um headset" width="132px" height="136px"/>
                 </picture>
 
@@ -19,14 +19,14 @@ function Dobra5() {
                     Contatos
                 </h3>
 
-                <div className='flex justify-around items-center gap-2'>
+                <div className='flex justify-around items-center gap-4'>
                     <AiFillInstagram className="w-[2em] h-[2em] fill-sub"/>
                     <AiFillFacebook className="w-[2em] h-[2em] fill-sub" />
                     <RiWhatsappFill className="w-[2em] h-[2em] fill-sub" />
                     <BsTelephoneFill className="w-[1.5em] h-[1.5em] fill-sub" />
                 </div>
 
-                <button className='rounded-xl bg-prim py-2 px-4 text-white font-bold text-[9px] mb-4'>Fale Conosco</button>
+                <button className='rounded-xl bg-prim py-2 px-4 text-white font-bold text-[10px] mb-4'>Fale Conosco</button>
 
             </div>
         </div>
